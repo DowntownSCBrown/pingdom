db4d8d778216ad6f4c1b02c4039df46c15018e4d
Steven Brown <69728772+DowntownSCBrown@users.noreply.github.com>
2020-09-02 20:28:30 -0400

    Create export-pingdom-uptime-checks.py

---
+++ export-pingdom-uptime-checks.py
@@ -0,0 +1,21 @@
+import requests
+import json
+import csv
+
+csvFile = 'C:\YouTube\PingdomChecks.csv'
+csvHead = ['Name', 'Status']
+
+api = "https://api.pingdom.com/api/3.1/checks"
+
+head = {'Authorization':'Bearer -Db5nZmkeyHfj3BFfyolq_EGhO9IwMdDJV41Hcc_8XUd_ks_ojSqlxxxxxxxxxxxxxxxxx'}
+
+pingdomChecks = requests.get(api, headers=head).json()
+
+with open(csvFile, 'w', newline='') as pChecksList:
+    csvwriter = csv.writer(pChecksList)
+    csvwriter.writerow(csvHead)
+
+    for check in pingdomChecks['checks']:
+        csvwriter.writerow((check['name'], check['status']))
+
+pChecksList.close()
